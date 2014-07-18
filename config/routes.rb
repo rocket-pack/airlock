496592d1198d3e775a5c4599868eb9568b66b865
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-19 11:43:58 +1200

    Install Doorkeeper, run generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Airlock::Application.routes.draw do
   devise_for :users
+  use_doorkeeper
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
