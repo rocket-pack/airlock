dca7d760eaf35ada34300d02ec63602d6f2169a4
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-19 12:40:53 +1200

    Don't require a user to sign in before authorising request

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -3,12 +3,9 @@ Doorkeeper.configure do
   # Currently supported options are :active_record, :mongoid2, :mongoid3, :mongo_mapper
   orm :active_record
 
-  # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    nil
+    # current_user || warden.authenticate!(:scope => :user)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
